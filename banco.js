22d222046e8a557036604c69cfc99fff69945c24
Isaac Oliveira <92345049+IsaacOliveiraSouza@users.noreply.github.com>
2022-11-03 16:12:23 -0300

    Delete banco.js

--- banco.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql2")
-const conn= mysql.createConnection({
-    host:"localhost",
-    user: "root",
-    password:"",
-    database : "redesocial"
-})
-module.exports = conn
-//dale
